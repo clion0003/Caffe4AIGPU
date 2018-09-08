43121055cb654a279f77d21ab879fc8a8df035f8
clion0003 <clion0003@gmail.com>
2018-09-09 05:01:55 +0800

    no meaning

--- src/caffe/layers/conv_layer.cu
+++ src/caffe/layers/conv_layer.cu
@@ -56,7 +56,7 @@ void ConvolutionLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     if (this->bias_term_ && this->param_propagate_down_[1]) {
       Dtype* bias_diff = this->blobs_[1]->mutable_gpu_diff();
       for (int n = 0; n < this->num_; ++n) {
-        this->backward_gpu_bias(bias_diff, top_diff + n * this->top_dim_);
+        //this->backward_gpu_bias(bias_diff, top_diff + n * this->top_dim_);
       }
     }
     if (this->param_propagate_down_[0] || propagate_down[i]) {
